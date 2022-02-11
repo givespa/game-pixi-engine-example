81c246c4d00942abb294868b94e13bb34a757113
Gian <givespas@gmail.com>
2022-02-11 18:05:39 -0500

    use keyCode

--- src/App.js
+++ src/App.js
@@ -10,14 +10,15 @@ import {
   ZoomTo,
   Camera,
 } from 'pixi-game-camera';
+import { keyboard } from "@testing-library/user-event/dist/keyboard";
 
 
 // let color = 'F04660';
 let color = 'Ffffff';
 
 const app = new PIXI.Application({
-  width: 1920,
-  height: 1080,
+  width: 1280,
+  height: 720,
   backgroundColor: `0x${color}`,
 });
 
@@ -25,12 +26,15 @@ const options = {
   ticker: app.ticker
 };
 
+const Keyboard = require("pixi.js-keyboard");
+
+
 const camera = new Camera(options);
+
 // const mildShake = new Shake(app.stage, 3, 5000);
 // camera.effect(mildShake);
 
-// const panToCenter = new PanTo(app.stage, 200, 250, 5000);
-// camera.effect(panToCenter);
+
 
 // const fadeToBlack = new Fade(app.stage, new PIXI.Sprite(PIXI.Texture.WHITE), 0x000000, 1, 5000);
 // camera.effect(fadeToBlack);
@@ -74,39 +78,79 @@ function onAssetsLoaded(loader, res) {
     // const zoomIn = new ZoomTo(spineBoyPro, 2, 2, 5000);
     // camera.effect(zoomIn);
 
+    // const panToCenter = new PanTo(spineBoyPro, 200, 250, 1000);
+    // camera.effect(panToCenter);
+
+    let animate = 'hoverboard'
+    let speed = 5;
+
     // set the position
+    // spineBoyPro.x = 0;
     spineBoyPro.x = app.screen.width / 2;
-    // spineBoyPro.y = app.screen.height;
     spineBoyPro.y = app.screen.height / 2;
 
     app.ticker.add((delta) => {
+
       // update the spine animation
-      // spineBoyPro.update(delta);
-      spineBoyPro.y += 9.8 * delta;
+      // console.log(delta);
+
+      if (Keyboard.isKeyDown("ArrowLeft")) {
+        spineBoyPro.x -= speed * delta;
+        // console.log('left');
+      }
+
+      if (Keyboard.isKeyDown("ArrowRight")) {
+        spineBoyPro.x += speed * delta;
+        // console.log('right');
+      }
+
+      Keyboard.update();
+      console.log(animate);
+      
     });
+    
+    Keyboard.events.on('pressed', (keyCode, e)  => {
+      // console.log(keyCode); 
+      if (keyCode === 'ArrowLeft') {
+        // console.log('left');
+        spineBoyPro.scale.x *= -1; //flip the sprite
+        animate = 'walk';
+        speed = 5;
+        spineBoyPro.state.setAnimation(0, animate, true);
+      }
+      if (keyCode === 'ArrowRight') {
+        // console.log('right');
+        animate = 'run';
+        speed = 10;
+        spineBoyPro.state.setAnimation(0, animate, true);
+    }});
+
+      
 
     spineBoyPro.scale.set(0.5);
 
     // app.stage.addChild(spineBoyPro);
     background.addChild(spineBoyPro);
 
-    const singleAnimations = ['aim', 'death', 'jump', 'portal'];
-    const loopAnimations = ['hoverboard', 'idle', 'run', 'shoot', 'walk'];
-    const allAnimations = [].concat(singleAnimations, loopAnimations);
+    // const singleAnimations = ['aim', 'death', 'jump', 'portal'];
+    // const loopAnimations = ['hoverboard', 'idle', 'run', 'shoot', 'walk'];
+    // const allAnimations = [].concat(singleAnimations, loopAnimations);
 
-    let lastAnimation = '';
+    spineBoyPro.state.setAnimation(0, animate, true);
+
+    // let lastAnimation = '';
 
     // Press the screen to play a random animation
-    app.stage.on('pointerdown', () => {
-        let animation = '';
-        do {
-            animation = allAnimations[Math.floor(Math.random() * allAnimations.length)];
-        } while (animation === lastAnimation);
+    // app.stage.on('pointerdown', () => {
+    //     let animation = '';
+    //     do {
+    //         animation = allAnimations[Math.floor(Math.random() * allAnimations.length)];
+    //     } while (animation === lastAnimation);
 
-        spineBoyPro.state.setAnimation(0, animation, loopAnimations.includes(animation));
+    //     spineBoyPro.state.setAnimation(0, animation, loopAnimations.includes(animation));
 
-        lastAnimation = animation;
-    });
+    //     lastAnimation = animation;
+    // });
 }
 
 function App() {
